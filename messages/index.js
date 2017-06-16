c8d64cd8467d9e625b83e3c6794ca210652d3652
Dale Kunce <dale@normalhabit.com>
2017-06-16 09:55:27 -0700

    more questions, adding location query

--- messages/index.js
+++ messages/index.js
@@ -8,6 +8,9 @@ var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var path = require('path');
 
+var locationDialog = require('botbuilder-location');
+bot.library(locationDialog.createLibrary(process.env['BingKey']);
+
 var useEmulator = (process.env.NODE_ENV == 'development');
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
@@ -20,22 +23,43 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
 var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
 
+
 bot.dialog('/', [
     function (session) {
         builder.Prompts.text(session, "Hello... What's your name?");
     },
     function (session, results) {
         session.userData.name = results.response;
+        locationDialog.getLocation(session, {
+          prompt: "What is your location",
+          useNativeControl: true,
+          reverseGeocode: true
+        });
+    },
+    function (session, results) {
+        session.userData.place = results.response;
         builder.Prompts.number(session, "Hi " + results.response + ", How many people are affected?");
     },
     function (session, results) {
         session.userData.pplaffected = results.response;
+        builder.Prompts.number(session, "How many people are injured?");
+    },
+    function (session, results) {
+        session.userData.pplinjured = results.response;
+        builder.Prompts.number(session, "How many people are dead?");
+    },
+    function (session, results) {
+        session.userData.ppldead = results.response;
         builder.Prompts.choice(session, "What are the major needs", ["Food", "Shelter", "Water", "Other"]);
+        //TODO this needs to be a multi-select question
     },
     function (session, results) {
         session.userData.needs = results.response.entity;
-        session.send("Got it... " + session.userData.name +
+        session.send("Thanks " + session.userData.name +
+                    " In " + session.userData.place.locality + session.userDate.place.region +
                     " People Affected: " + session.userData.pplaffected +
+                    " Injured: " + session.userData.pplinjured +
+                    " Deceased: " + session.userData.ppldead +
                     " Major Needs: " + session.userData.needs +  ".");
     }
 ]);
