7bfe655f302c877021ffc745ac833070c0fead1f
Dale Kunce <dale@normalhabit.com>
2017-06-15 22:54:26 -0700

    ish working bot:

---
+++ messages/index.js
@@ -0,0 +1,52 @@
+/*-----------------------------------------------------------------------------
+This template demonstrates how to use Waterfalls to collect input from a user using a sequence of steps.
+For a complete walkthrough of creating this type of bot see the article at
+https://aka.ms/abs-node-waterfall
+-----------------------------------------------------------------------------*/
+"use strict";
+var builder = require("botbuilder");
+var botbuilder_azure = require("botbuilder-azure");
+var path = require('path');
+
+var useEmulator = (process.env.NODE_ENV == 'development');
+
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword'],
+    stateEndpoint: process.env['BotStateEndpoint'],
+    openIdMetadata: process.env['BotOpenIdMetadata']
+});
+
+var bot = new builder.UniversalBot(connector);
+bot.localePath(path.join(__dirname, './locale'));
+
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, "Hello... What's your name?");
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.number(session, "Hi " + results.response + ", How many people are affected?");
+    },
+    function (session, results) {
+        session.userData.pplaffected = results.response;
+        builder.Prompts.choice(session, "What are the major needs", ["Food", "Shelter", "Water", "Other"]);
+    },
+    function (session, results) {
+        session.userData.needs = results.response.entity;
+        session.send("Got it... " + session.userData.name +
+                    " People Affected: " + session.userData.pplaffected +
+                    " Major Needs: " + session.userData.needs +  ".");
+    }
+]);
+
+if (useEmulator) {
+    var restify = require('restify');
+    var server = restify.createServer();
+    server.listen(3978, function() {
+        console.log('test bot endpont at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());
+} else {
+    module.exports = { default: connector.listen() }
+}
